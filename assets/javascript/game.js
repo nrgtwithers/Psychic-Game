df056e178fb615a394f6b682348b9bf4739aa1d3
Nina Withers <ninarosewithers@gmail.com>
2018-12-06 19:48:44 -0500

    Changed Css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,37 @@
 // Variables
 
-var LetterArr = ["a", "b", "c","d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n","o","p","q","r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var randomLetter = Math.floor(Math.random() * LetterArr.length);
-var randomLet = LetterArr[randomLetter];
+var compChoices = ["a", "b", "c","d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n","o","p","q","r", "s", "t", "u", "v", "w", "x", "y", "z"]
+// var compGuess = compChoices[randomLetter];
 var wins = 0;
 var losses = 0;
-var guessesLeft = 0;
-var guessSoFar =[];
+var guessesLeft = 10;
+var guessSoFar = [];
 
-//Test
-console.log(LetterArr);
-console.log(randomLet);
+document.onkeyup = function(event){
+
+var userGuess = event.key;
+var compGuess = compChoices[Math.floor(Math.random() * 26)];
+
+
+
+// wins
+if (userGuess == compChoices){
+    console.log(userGuess);
+    console.log(wins);
+    wins++;
+//losses
+} else if (userGuess != compChoices){
+    // console.log(compChoices);
+    // console.log(losses);
+    losses++;
+}
+
 
 // DOM - connect to html
 
+var userScoreDiv = document.getElementById('wins');
+userScoreDiv.textContent = wins;
+
+var compScoreDiv = document.getElementById('losses');
+compScoreDiv.textContent = losses;
+};
